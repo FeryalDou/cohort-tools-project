955094504da16432cfb9b62aa3c4d66f4ea8049c
Feryal Doubali <feryalhalima@gmail.com>
2024-04-09 17:02:07 +0200

    fix err

--- server/routes/students.routes.js
+++ server/routes/students.routes.js
@@ -1,20 +1,29 @@
 const router = require("express").Router();
-const cohorts = require("../students.json");
-
+const mongoose = require("mongoose");
+const Student = require("../models/Student.model");
 //all the routes are prefixed with /api/students
 
 router.get("/", (req, res) => {
-  res.json(students);
+  Student.find({})
+    .then((students) => {
+      res.json(students);
+    })
+    .catch((error) => {
+      console.error("error while retreiving students", error);
+      res.status(500).send({ error: "failed to retreive students" });
+    });
 });
 
 router.get("/:studentId", (req, res, next) => {
-  const id = Number(req.params.studentId);
-  const oneStudent = students.find((element) => element._id === id);
-  if (oneStudent) {
-    res.json(oneStudent);
-  } else {
-    res.status(400).json({ message: `${id} not found` });
-  }
+  // const id = Number(req.params.studentId);
+  Student.findById(req.params.studentId)
+    .then((oneStudent) => {
+      res.json(oneStudent);
+    })
+    .catch((err) => {
+      console.error("error while retreiving students", err);
+      res.json({ error: "error on the backend, see console" });
+    });
 });
 
 module.exports = router;
