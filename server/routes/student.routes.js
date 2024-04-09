98dfa5fda5a4d750d1ad9e18db3da5fef9092214
Feryal Doubali <feryalhalima@gmail.com>
2024-04-09 16:05:41 +0200

    cleaning up

---
+++ server/routes/student.routes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const students = require("./../students.json");
+
+//all the routes are prefixed with /api/students
+
+router.get("/", (req, res) => {
+  res.json(students);
+});
+
+router.get("/:studentId", (req, res, next) => {
+  const id = Number(req.params.studentId);
+  const oneStudent = students.find((element) => element._id === id);
+  if (oneStudent) {
+    res.json(oneStudent);
+  } else {
+    res.status(400).json({ message: `${id} not found` });
+  }
+});
+
+module.exports = router;
