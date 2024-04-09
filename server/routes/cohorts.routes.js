8f5b5ec598a19ce2c181b9f78083fa0296ca4a7e
Feryal Doubali <feryalhalima@gmail.com>
2024-04-09 17:09:51 +0200

    routes done

--- server/routes/cohorts.routes.js
+++ server/routes/cohorts.routes.js
@@ -1,19 +1,30 @@
 const router = require("express").Router();
 const cohorts = require("./../cohorts.json");
+const Cohort = require("../models/Student.model");
 
 //all the routes are prefixed with /api/cohorts
 
 router.get("/", (req, res) => {
-  res.json(cohorts);
+  Cohort.find({})
+    .then((cohorts) => {
+      res.json(cohorts);
+    })
+    .catch((error) => {
+      console.error("error while retreiving cohorts", error);
+      res.status(500).send({ error: "failed to retreive cohort" });
+    });
 });
 
 router.get("/:cohortId", (req, res, next) => {
-  const id = Number(req.params.cohortId);
-  const oneCohort = cohorts.find((element) => element._id === id);
-  if (oneCohort) {
-    res.json(oneCohort);
-  } else {
-    res.status(400).json({ message: `${id} not found` });
-  }
+  //const id = Number(req.params.cohortId);
+  Cohort.findById(req.params.cohortId)
+    .then((oneCohort) => {
+      res.json(oneCohort);
+    })
+    .catch((err) => {
+      console.error("error while retreiving cohorts", err);
+      res.json({ error: "error on the backend, see console" });
+    });
 });
+
 module.exports = router;
