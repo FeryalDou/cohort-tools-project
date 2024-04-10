7b0a065b718fc5b7992e7715894d66a3e2ebf15b
Feryal Doubali <feryalhalima@gmail.com>
2024-04-10 11:04:49 +0200

    check differencies

--- server/routes/cohorts.routes.js
+++ server/routes/cohorts.routes.js
@@ -3,6 +3,14 @@ const cohorts = require("./../cohorts.json");
 const Cohort = require("../models/Student.model");
 
 //all the routes are prefixed with /api/cohorts
+
+/**
+ * what's in a request?
+ * request body:hold some informations
+ * request params: a part of the url which can change
+ * request query: search params or querystrings
+ */
+
 // router.get("/", (req, res) => {
 //   Cohort.find({})
 //     .then((cohorts) => {
@@ -13,15 +21,27 @@ const Cohort = require("../models/Student.model");
 //       res.status(500).send({ error: "failed to retreive cohort" });
 //     });
 //});
+
 router.get("/", async (req, res) => {
   try {
-    const allCohorts = await Cohort.find({});
+    console.log(req.query);
+    let skip = 0;
+    let limit = 2;
+
+    if (req.query.skip !== undefined) {
+      skip = Number(req.query.skip);
+    }
+    if (req.query.limit !== undefined) {
+      limit = Number(req.query.limit);
+    }
+
+    const allCohorts = await Cohort.find({}).limit(limit).skip(skip);
     res.json({
       results: allCohorts,
     });
   } catch (error) {
     console.log(error);
-    res.send("Error during GET/api/cohorts");
+    res.status(500).send("Error during GET/api/cohorts");
   }
 });
 
--- server/routes/students.routes.js
+++ server/routes/students.routes.js
@@ -2,16 +2,45 @@ const router = require("express").Router();
 const mongoose = require("mongoose");
 const Student = require("../models/Student.model");
 //all the routes are prefixed with /api/students
+/**
+ * what's in a request?
+ * request body:hold some informations
+ * request params: a part of the url which can change
+ * request query: search params or querystrings
+ */
 
-router.get("/", (req, res) => {
-  Student.find({})
-    .then((students) => {
-      res.json(students);
-    })
-    .catch((error) => {
-      console.error("error while retreiving students", error);
-      res.status(500).send({ error: "failed to retreive students" });
+// router.get("/", (req, res) => {
+//   Student.find({})
+//     .then((students) => {
+//       res.json(students);
+//     })
+//     .catch((error) => {
+//       console.error("error while retreiving students", error);
+//       res.status(500).send({ error: "failed to retreive students" });
+//     });
+// });
+
+router.get("/", async (req, res) => {
+  try {
+    console.log(req.query);
+    let skip = 0;
+    let limit = 2;
+
+    if (req.query.skip !== undefined) {
+      skip = Number(req.query.skip);
+    }
+    if (req.query.limit !== undefined) {
+      limit = Number(req.query.limit);
+    }
+
+    const allStudent = await Student.find({}).limit(limit).skip(skip);
+    res.json({
+      results: allStudent,
     });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error during GET/api/students");
+  }
 });
 
 router.get("/:studentId", (req, res, next) => {
